062d7047fcedc513d53e5338da2df1eb89f5b98d
Adriana Lima <adrianalimafm@gmail.com>
2020-07-27 23:18:36 -0300

    Upload de Imagens e Tratativa de erros

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
-import 'express-async-errors';
+import 'express-async-errors'; // tratativa de erros
 
 import routes from './routes';
 import uploadConfig from './config/upload';
@@ -12,12 +12,12 @@ import './database';
 const app = express();
 
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.directory)); // permite visualizar o avatar no navegador pelo localhost
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 // tratativa global de erros das rotas
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof  AppError){
+    if (err instanceof AppError){
         return response.status(err.statusCode).json({
             status: 'error',
             message: err.message,
