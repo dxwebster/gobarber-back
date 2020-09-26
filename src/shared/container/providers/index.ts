2294490e81602a9760b0f9a94c9242eefd9f1afe
dxwebster <adrianalimafm@gmail.com>
2020-09-25 21:16:41 -0300

    Add Tempalte Email

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,10 +1,20 @@
 import { container } from 'tsyringe';
 
+// Registro de Providers para dependency injection
+// Sempre que for implementar uma biblioteca externa é necessário utilizar esse dependency injection
+// Pois é um serivço que pode mudar
+
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
 import IMailProvider from './MailProvider/models/IMailProvider';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
 
+import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
+import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
+
 container.registerSingleton<IStorageProvider>('StorageProvicer', DiskStorageProvider);
-container.registerInstance<IMailProvider>('MailProvider', new EtherealMailProvider());
+
+container.registerSingleton<IMailTemplateProvider>('MailTemplateProvider', HandlebarsMailTemplateProvider);
+
+container.registerInstance<IMailProvider>('MailProvider', container.resolve(EtherealMailProvider));
