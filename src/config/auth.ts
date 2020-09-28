8645feea2ac734620b2adccf5f34a8871d1651fd
dxwebster <adrianalimafm@gmail.com>
2020-09-27 22:21:51 -0300

    Updates

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
-    jwt: {
-        secret: '93a94a37c5da3a15468b05df3a9d1a5b',
-        expiresIn: '10d',
-    },
+  jwt: {
+    secret: process.env.APP_SECRET,
+    expiresIn: '10d',
+  },
 };
